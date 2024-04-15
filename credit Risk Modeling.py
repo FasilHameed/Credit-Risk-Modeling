80298bb27f5193a902df4117c574d8810ee5d4a8
Fasil hameed <faisalhameed763@gmail.com>
2024-04-15 20:00:04 +0530

    combined datasets

--- credit Risk Modeling.py
+++ credit Risk Modeling.py
@@ -1,3 +1,4 @@
+# Importing necessary libraries
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -10,43 +11,41 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier,
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 from catboost import CatBoostClassifier
-from warnings import filterwarnings
-filterwarnings("ignore")
 import os
+from warnings import filterwarnings
+
+# Ignoring warnings
+filterwarnings("ignore")
 
 
 # Load Dataset
-a1=pd.read_excel('case_study1.xlsx')
-a2=pd.read_excel('case_study2.xlsx')
+# Read the excel files
+a1 = pd.read_excel('case_study1.xlsx')
+a2 = pd.read_excel('case_study2.xlsx')
 
+# Make Copies of the dataset
+df1 = a1.copy()
+df2 = a2.copy()
 
-# make Copies of the dataset
-df1=a1.copy()
-df2=a2.copy()
+# Remove nulls from df1
+# Drop rows where 'Age_Oldest_TL' is -99999
+df1 = df1.loc[df1['Age_Oldest_TL'] != -99999]
 
-
-# Remove nulls
-df1=df1.loc[df1['Age_Oldest_TL']!=-99999]
-
-
-
-# removing columns with more Null values
-
-columns_to_be_removed=[]
+# Removing columns with more Null values from df2
+columns_to_be_removed = []
+# Identify columns with more than 10000 null values
 for i in df2.columns:
-    if df2.loc[df2[i]==-99999].shape[0] > 10000:
+    if df2.loc[df2[i] == -99999].shape[0] > 10000:
         columns_to_be_removed.append(i)
 
-df2=df2.drop(columns_to_be_removed,axis=1)
+# Drop identified columns
+df2 = df2.drop(columns_to_be_removed, axis=1)
 
-
-# removing nulls from all columns
-
+# Removing nulls from all columns in df2
 for i in df2.columns:
-    df2=df2.loc[df2[i] !=-99999]
+    df2 = df2.loc[df2[i] != -99999]
 
-
-
-
+# combining datasets
+df=pd.merge(df1,df2,on=['PROSPECTID'],how='inner')
 
 
