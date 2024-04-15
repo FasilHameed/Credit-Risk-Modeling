a2550527b8c6b75e8b95aab5f004cd7e35107b78
Fasil hameed <faisalhameed763@gmail.com>
2024-04-15 19:40:59 +0530

    removed null values

--- credit Risk Modeling.py
+++ credit Risk Modeling.py
@@ -16,13 +16,37 @@ import os
 
 
 # Load Dataset
-
 a1=pd.read_excel('case_study1.xlsx')
 a2=pd.read_excel('case_study2.xlsx')
+
 
 # make Copies of the dataset
 df1=a1.copy()
 df2=a2.copy()
+
+
+# Remove nulls
+df1=df1.loc[df1['Age_Oldest_TL']!=-99999]
+
+
+
+# removing columns with more Null values
+
+columns_to_be_removed=[]
+for i in df2.columns:
+    if df2.loc[df2[i]==-99999].shape[0] > 10000:
+        columns_to_be_removed.append(i)
+
+df2=df2.drop(columns_to_be_removed,axis=1)
+
+
+# removing nulls from all columns
+
+for i in df2.columns:
+    df2=df2.loc[df2[i] !=-99999]
+
+
+
 
 
 
